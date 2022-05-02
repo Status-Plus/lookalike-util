d901b5822b38558883d04d2ffb8f76fcdc156335
Ethan <ethan@fxllencode.dev>
2022-05-02 13:14:51 -0400

    webpack

--- tests/doreplace.test.js
+++ tests/doreplace.test.js
@@ -1,7 +1,15 @@
 import { clearSimilar } from '../src/index'
 
-test('replace ', () => {
-    let testString = clearSimilar("Hello, World!")
+test('replace ü with u ', () => {
+    let testString = clearSimilar("you sück!")
 
-    expect(testString).toBe("Hello, World!")
+    expect(testString).toBe("you suck!")
 })
+
+test('replace multiple non-english characters with their counterparts', () => {
+    let testString = clearSimilar("Êċℎọ!")
+
+    expect(testString).toBe("Echo!")
+})
+
+
